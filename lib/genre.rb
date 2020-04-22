b3230a8c9758d8ecfe24242d1989aadd76f8952f
Allan Farca <allan.farca@gmail.com>
2020-04-22 04:28:03 +0000

    Done.

---
+++ lib/genre.rb
@@ -0,0 +1,23 @@
+class Genre 
+  attr_accessor :genre
+  
+  @@all = []
+  
+  def initialize (genre)
+    @genre = genre 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def songs 
+    
+  end 
+  
+  def artists
+    
+  end 
+  
+end 
