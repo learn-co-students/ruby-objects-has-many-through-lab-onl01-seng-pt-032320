648ebce8c4f521c4a22c6d8148d883f46c1b72c9
Hannah Gouin <hannah.l.gouin@gmail.com>
2020-04-24 06:06:53 +0000

    Done.

---
+++ lib/patient.rb
@@ -0,0 +1,31 @@
+class Patient
+  @@all = []
+
+  attr_reader :name, :doctor, :appointment, :date
+
+  def initialize(name)
+    @name = name
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(date, doctor)
+    new_appointment = Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.patient == self
+    end
+  end
+
+  def doctors
+    appointments.map do |appointment|
+      appointment.doctor
+    end
+  end
+end
